fed9c05eb6aa1f871078939fcfa08d54e89ae02c
Gabriela Alejandra Bustamante Medina <gabustamantemedina@gmail.com>
2025-05-12 14:30:36 -0600

    Jump Fixed
    Jump function fixed, for customize the jump just change gravity and jump height in the constructor.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,7 +8,6 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float _movementSpeed = 6f;
     [SerializeField] private float _jumpHeight = 10f;
     [SerializeField] private float _gravity = 9.81f;
-    [SerializeField] private float _doubleJumpMultiplier = 3f;
     private Vector3 _direction;
     private float _directionY;
     private bool _canDoubleJump = false;
@@ -37,16 +36,15 @@ public class PlayerController : MonoBehaviour
 
     void Movement()
     {
-        //fix that when you rotate the axis ar moving as well 
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
         _direction = horizontal * transform.right + vertical * transform.forward;
+        
         
     }
 
     void Rotation()
     {
-        //Rotation 
         float mouseX = Input.GetAxis("Mouse X");//left and right
         float mouseY = Input.GetAxis("Mouse Y");// up and down
       
@@ -60,24 +58,15 @@ public class PlayerController : MonoBehaviour
 
     void Jump()
     {
-        //Jump
-        if (_controller.isGrounded) //this helps to end the infinity jumps 
+        if (Input.GetButtonDown ("Jump") && _controller.isGrounded)
         {
-            _canDoubleJump = true;
-            if (Input.GetButtonDown("Jump"))
-            {
-                _directionY = _jumpHeight; 
-            }
+            _directionY = _jumpHeight; 
         }
-        else if (Input.GetButtonDown("Jump") && _canDoubleJump)
-        {
-            _directionY = _jumpHeight*_doubleJumpMultiplier;
-            _canDoubleJump = false;
-        }
-
-        _directionY -= _gravity;//smooth the jump;
+        //Apply Gravity 
+        _directionY-= _gravity * Time.deltaTime;
         _direction.y = _directionY;
-       _controller.Move(_direction * _movementSpeed * Time.deltaTime);
+        _controller.Move(_direction * _movementSpeed * Time.deltaTime);
+        
     }
     
 }
