8b6183a09c175feefe3c49248aa19612df9cce38
Gabriela Alejandra Bustamante Medina <gabustamantemedina@gmail.com>
2025-08-15 10:59:35 -0600

    ShootEnemies Done
    -Fixed the bullets prefab for the player and the enemy, in the layers collision; they will not collide with the bullets or the collectables.
    
    -Summarized  the code for the enemy. The update was getting messy so created a look target function that updates the position of the enemy to always look at the player location.
    
    -Build the project and everything is working for now.

--- Assets/Scripts/Enemy/ShootingEnemy.cs
+++ Assets/Scripts/Enemy/ShootingEnemy.cs
@@ -1,87 +1,74 @@
 
 using System.Collections;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class ShootingEnemy : Enemy
 {
    [SerializeField] private Transform shootingPoint;
    [SerializeField] private GameObject bulletPrefab;
-   [SerializeField] private float fireRate = 5;
-   private float targetDistance;
    [SerializeField] private float turnSpeed = 10f;
    [SerializeField] private float aimHeight = 1.2f;
+   [SerializeField] private int burstCount = 3;
+   [SerializeField] private float burstInterval = 0.12f;
    private bool _canShoot = false;
+   [SerializeField] private float lastShot= 1;
+   [SerializeField] private float timeForShoot= 1;
    
    private void Awake()
    {
          _currentTarget = GameManager.playerInstance;
-         _canShoot = true; 
+         _canShoot = false; 
          
    }
    
-   private void Update ()
+   private void Update()
    {
-       LookAtTarget(); 
-
-       if (_canShoot && IsTargetInDetection())
+       LookAtTarget();
+       // Shoot after time has passed 
+       bool timeComplete = Time.time >= lastShot + timeForShoot;
+       if (_canShoot && timeComplete )
        {
-           ShootOnce();
-           StartCoroutine(ShootCooldown());
+           lastShot = Time.time;
+           StartCoroutine(FireBurst());
        }
-
    }
-   private bool IsTargetInDetection()
+
+   private IEnumerator FireBurst()
    {
-       if (_currentTarget == null) return false;
-       float targetDistance = Vector3.Distance(transform.position, _currentTarget.transform.position);
-       return targetDistance <= detectionDistance;
+       for (int i = 0; i < burstCount; i++)
+       {
+           Fire();
+           yield return new WaitForSeconds(burstInterval);
+       }
+   }
+
+   private void Fire()
+   {
+       if (_canShoot)
+       {
+           Vector3 direction = (_currentTarget.transform.position + Vector3.up * aimHeight) - shootingPoint.position;
+           Instantiate(bulletPrefab, shootingPoint.position, Quaternion.LookRotation(direction));
+
+       }
    }
 
    private void LookAtTarget()
    {
-       if (_currentTarget == null) return;
-       Vector3 directionTarget = (_currentTarget.transform.position - transform.position);
-       directionTarget.y = 0f; //will rotate horizontally 
-       if (directionTarget.sqrMagnitude > 0.001f) //Stops the enemy to arrive to 0 
-       {
-           transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(directionTarget), turnSpeed * Time.deltaTime);
-       }
+       Vector3 targetPosition = new Vector3(_currentTarget.transform.position.x, transform.position.y, _currentTarget.transform.position.z);
+
+       float playerDistance = Vector3.Distance(transform.position, targetPosition);
+
+       _canShoot = playerDistance <= detectionDistance;
+       Vector3 lookDirection = targetPosition - transform.position;
+       float enemyRotation = turnSpeed * Time.deltaTime;
+       Vector3 newLookDirection = Vector3.RotateTowards(transform.forward, lookDirection, enemyRotation, 0f);
+       transform.rotation = Quaternion.LookRotation(newLookDirection);//updates the look of the enemy towards the player
    }
 
-   private void ShootOnce()
-   {
-       if (bulletPrefab == null || shootingPoint == null || _currentTarget == null) return;
-
-       Vector3 shootingDistance = (_currentTarget.transform.position + Vector3.up *aimHeight) - shootingPoint.position;
-       bulletPrefab = Instantiate(bulletPrefab, shootingPoint.position, Quaternion.LookRotation(shootingDistance));
-
-   }
-
-  IEnumerator ShootCooldown()
-   {
-       _canShoot = false;
-       yield return new WaitForSeconds(fireRate);
-       _canShoot = true;
-   }
    private void OnDrawGizmosSelected()
    {
-       Gizmos.color = Color.cyan;
+       Gizmos.color = Color.red;
        Gizmos.DrawWireSphere(transform.position, detectionDistance);
-       
-       if (shootingPoint != null)
-       {
-           Gizmos.color = Color.yellow;
-           Gizmos.DrawSphere(shootingPoint.position, 0.06f);
-       }
-       
-       if (_currentTarget != null && shootingPoint != null)
-       {
-           Vector3 aimPoint = _currentTarget.transform.position + Vector3.up * aimHeight;
-
-           Gizmos.color = Color.red;
-           Gizmos.DrawLine(shootingPoint.position, aimPoint);
-           Gizmos.DrawSphere(aimPoint, 0.06f);
-       }
    }
 }
-
